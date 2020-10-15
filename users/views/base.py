34709513c7bc8c7a54e4e3a0de44ebb09e8ebbb3
ted <tngeene27@gmail.com>
2020-10-15 14:16:16 +0300

    fixed lab sec redirection

--- users/views/base.py
+++ users/views/base.py
@@ -20,7 +20,7 @@ def login_redirect(request):
     elif request.user.user_type == 'Lab_Tech':
         return redirect('users:lab_technicians_dashboard_index')
     elif request.user.user_type == 'Lab_Sec':
-        return redirect('users:lab_sec_dashoard_index')
+        return redirect('users:lab_sec_dashboard_index')
     elif request.user.is_staff:
         return redirect('dashboard:index')
 
